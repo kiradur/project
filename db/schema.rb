ce944a0cfe5e5fda1aed2f680bd6bd065123fbfa
Кирилл Войтулевич <voytulevich.k@gmail.com>
2020-01-27 16:04:10 +0300

    registation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_075531) do
+ActiveRecord::Schema.define(version: 2020_01_27_121848) do
 
   create_table "client_cards", force: :cascade do |t|
     t.text "title"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_01_27_075531) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
